b0ca464ebb2d715c075f4f31be462a8871a75e80
Dirk Kröhan <dirk.kroehan@qaware.de>
2022-09-22 11:17:23 +0200

    Initialize project with start.spring.io

---
+++ src/main/java/de/qaware/demo/jcon22/caching/CachingDemoApplication.java
@@ -0,0 +1,13 @@
+package de.qaware.demo.jcon22.caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/qaware/demo/jcon22/caching/CachingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.qaware.demo.jcon22.caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
